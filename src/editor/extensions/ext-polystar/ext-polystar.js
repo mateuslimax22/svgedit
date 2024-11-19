99fd7a402f593334c7bea9b0c93b64bc1f1b2c8e
Mateus Lima <mateus@knowcode.app>
2024-11-19 08:00:56 -0300

    add 3 star

--- src/editor/extensions/ext-polystar/ext-polystar.js
+++ src/editor/extensions/ext-polystar/ext-polystar.js
@@ -81,12 +81,14 @@ export default {
         const fbtitle = `${name}:title`
         const titleStar = `${name}:buttons.0.title`
         const titlePolygon = `${name}:buttons.1.title`
+        const titleStarlight = `${name}:buttons.2.title`;
         const buttonTemplate = `
             <se-flyingbutton id="tools_polygon" title="${fbtitle}">
               <se-button id="tool_star" title="${titleStar}" src="star.svg">
               </se-button>
-              <se-button id="tool_polygon" title="${titlePolygon}" src="polygon.svg">
+              <se-button id="tool_polygon" title="${titlePolygon}" src="starBold.svg">
               </se-button>
+               <se-button id="tool_starlight" title="${titleStarlight}" src="starLight.svg"></se-button>
             </se-flyingbutton>
           `
         svgCanvas.insertChildAtIndex($id('tools_left'), buttonTemplate, 10)
@@ -96,6 +98,7 @@ export default {
             svgCanvas.setMode('star')
             showPanel(true, 'star')
             showPanel(false, 'polygon')
+            showPanel(false, 'starlight');
           }
         })
         $click($id('tool_polygon'), () => {
@@ -103,8 +106,18 @@ export default {
             svgCanvas.setMode('polygon')
             showPanel(true, 'polygon')
             showPanel(false, 'star')
+            showPanel(false, 'starlight');
           }
         })
+
+        $click($id('tool_starlight'), () => {
+          if (this.leftPanel.updateLeftPanel('tool_starlight')) {
+            svgCanvas.setMode('starlight');
+            showPanel(false, 'star');
+            showPanel(false, 'polygon');
+            showPanel(false, 'starlight');
+          }
+        });
         const label0 = `${name}:contextTools.0.label`
         const title0 = `${name}:contextTools.0.title`
         const label1 = `${name}:contextTools.1.label`
@@ -128,12 +141,16 @@ export default {
             <se-spin-input size="3" id="polySides" min=1 step=1 value=5 label="${label3}" title="${title3}">
             </se-spin-input>
           </div>
+          <div id="starlight_panel">
+            <se-spin-input id="starlightIntensity" label="Intensity" min=0 step=0.1 value=1 title="Adjust the starlight intensity"></se-spin-input>
+          </div>
         `
         // add handlers for the panel
         $id('tools_top').appendChild(panelTemplate.content.cloneNode(true))
         // don't display the panels on start
         showPanel(false, 'star')
         showPanel(false, 'polygon')
+        showPanel(false, 'starlight');
         $id('starNumPoints').addEventListener('change', (event) => {
           setAttr('point', event.target.value)
           const orient = 'point'
@@ -201,6 +218,9 @@ export default {
         $id('radialShift').addEventListener('change', (event) => {
           setAttr('radialshift', event.target.value)
         })
+        $id('starlightIntensity').addEventListener('change', (event) => {
+          setAttr('intensity', event.target.value);
+        });
         $id('polySides').addEventListener('change', (event) => {
           setAttr('sides', event.target.value)
           const sides = event.target.value
@@ -240,6 +260,54 @@ export default {
         })
       },
       mouseDown(opts) {
+        if (svgCanvas.getMode() === 'starlight') {
+          const fill = svgCanvas.getColor('fill');
+          const stroke = svgCanvas.getColor('stroke');
+          const strokeWidth = 2;
+          started = true;
+
+          // Define as coordenadas iniciais
+          startX = opts.start_x;
+          startY = opts.start_y;
+
+          // Cria o SVG do 'X' usando duas linhas
+          newFO = svgCanvas.addSVGElementsFromJson({
+            element: 'g', // agrupa as duas linhas
+            attr: {
+              id: svgCanvas.getNextId(),
+              shape: 'xShape' // novo tipo de forma
+            },
+            children: [
+              {
+                element: 'line',
+                attr: {
+                  x1: startX,
+                  y1: startY,
+                  x2: startX,
+                  y2: startY, stroke,
+                  'stroke-width': strokeWidth,
+                  'stroke-linecap': 'round'
+                }
+              },
+              {
+                element: 'line',
+                attr: {
+                  x1: startX,
+                  y1: startY,
+                  x2: startX,
+                  y2: startY,
+                  stroke,
+                  'stroke-width': strokeWidth,
+                  'stroke-linecap': 'round'
+                }
+              }
+            ]
+          });
+          return {
+            started: true
+          }
+        }
+
         if (svgCanvas.getMode() === 'star') {
           const fill = svgCanvas.getColor('fill');
           const stroke = svgCanvas.getColor('stroke');
@@ -288,26 +356,49 @@ export default {
           }
         }
         if (svgCanvas.getMode() === 'polygon') {
-          const fill = svgCanvas.getColor('fill')
-          const stroke = svgCanvas.getColor('stroke')
-          const strokeWidth = svgCanvas.getStrokeWidth()
-          started = true
-          newFO = svgCanvas.addSVGElementsFromJson({
-            element: 'polygon',
-            attr: {
-              cx: opts.start_x,
-              cy: opts.start_y,
-              id: svgCanvas.getNextId(),
-              shape: 'regularPoly',
-              sides: $id('polySides').value,
-              orient: 'x',
-              edge: 0,
-              fill,
-              stroke,
-              'stroke-width': strokeWidth
-            }
-          })
+          const fill = svgCanvas.getColor('fill');
+          const stroke = svgCanvas.getColor('stroke');
+          const strokeWidth = 20;
+          started = true;
 
+          // Define as coordenadas iniciais
+          startX = opts.start_x;
+          startY = opts.start_y;
+
+          // Cria o SVG do 'X' usando duas linhas
+          newFO = svgCanvas.addSVGElementsFromJson({
+            element: 'g', // agrupa as duas linhas
+            attr: {
+              id: svgCanvas.getNextId(),
+              shape: 'xShape' // novo tipo de forma
+            },
+            children: [
+              {
+                element: 'line',
+                attr: {
+                  x1: startX,
+                  y1: startY,
+                  x2: startX,
+                  y2: startY, stroke,
+                  'stroke-width': strokeWidth,
+                  'stroke-linecap': 'round'
+                }
+              },
+              {
+                element: 'line',
+                attr: {
+                  x1: startX,
+                  y1: startY,
+                  x2: startX,
+                  y2: startY,
+                  stroke,
+                  'stroke-width': strokeWidth,
+                  'stroke-linecap': 'round'
+                }
+              }
+            ]
+          });
+          
           return {
             started: true
           }
@@ -317,6 +408,30 @@ export default {
       mouseMove(opts) {
         if (!started) {
           return undefined
+        }
+        if (svgCanvas.getMode() === 'starlight') {
+          // Calcula a distância do ponto inicial ao ponto atual do mouse
+          const x = opts.mouse_x;
+          const y = opts.mouse_y;
+          const deltaX = x - startX;
+          const deltaY = y - startY;
+          const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+          // Define o tamanho do 'X' com base na distância
+          newFO.firstChild.setAttribute('x1', startX - distance);
+          newFO.firstChild.setAttribute('y1', startY - distance);
+          newFO.firstChild.setAttribute('x2', startX + distance);
+          newFO.firstChild.setAttribute('y2', startY + distance);
+
+          newFO.lastChild.setAttribute('x1', startX + distance);
+          newFO.lastChild.setAttribute('y1', startY - distance);
+          newFO.lastChild.setAttribute('x2', startX - distance);
+          newFO.lastChild.setAttribute('y2', startY + distance);
+
+
+          return {
+            started: true
+          }
         }
         if (svgCanvas.getMode() === 'star') {
           // Calcula a distância do ponto inicial ao ponto atual do mouse
@@ -343,37 +458,23 @@ export default {
           }
         }
         if (svgCanvas.getMode() === 'polygon') {
-          const cx = Number(newFO.getAttribute('cx'))
-          const cy = Number(newFO.getAttribute('cy'))
-          const sides = Number(newFO.getAttribute('sides'))
-          // const orient = newFO.getAttribute('orient');
-          const fill = newFO.getAttribute('fill')
-          const stroke = newFO.getAttribute('stroke')
-          const strokeWidth = Number(newFO.getAttribute('stroke-width'))
+          const x = opts.mouse_x;
+          const y = opts.mouse_y;
+          const deltaX = x - startX;
+          const deltaY = y - startY;
+          const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 
-          let x = opts.mouse_x
-          let y = opts.mouse_y
+          // Define o tamanho do 'X' com base na distância
+          newFO.firstChild.setAttribute('x1', startX - distance);
+          newFO.firstChild.setAttribute('y1', startY - distance);
+          newFO.firstChild.setAttribute('x2', startX + distance);
+          newFO.firstChild.setAttribute('y2', startY + distance);
 
-          const edg =
-            Math.sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy)) / 1.5
-          newFO.setAttribute('edge', edg)
+          newFO.lastChild.setAttribute('x1', startX + distance);
+          newFO.lastChild.setAttribute('y1', startY - distance);
+          newFO.lastChild.setAttribute('x2', startX - distance);
+          newFO.lastChild.setAttribute('y2', startY + distance);
 
-          const inradius = (edg / 2) * cot(Math.PI / sides)
-          const circumradius = inradius * sec(Math.PI / sides)
-          let points = ''
-          for (let s = 0; sides >= s; s++) {
-            const angle = (2.0 * Math.PI * s) / sides
-            x = circumradius * Math.cos(angle) + cx
-            y = circumradius * Math.sin(angle) + cy
-
-            points += x + ',' + y + ' '
-          }
-
-          // const poly = newFO.createElementNS(NS.SVG, 'polygon');
-          newFO.setAttribute('points', points)
-          newFO.setAttribute('fill', fill)
-          newFO.setAttribute('stroke', stroke)
-          newFO.setAttribute('stroke-width', strokeWidth)
           return {
             started: true
           }
@@ -381,6 +482,18 @@ export default {
         return undefined
       },
       mouseUp() {
+        if (svgCanvas.getMode() === 'starlight') {
+          const selectToolButton = document.getElementById('tool_select');
+          if (selectToolButton) {
+            selectToolButton.click();
+          }
+          const sl = newFO.getAttribute('sl');
+          return {
+            keep: sl !== '0',
+            element: newFO
+          };
+          
+        }
         if (svgCanvas.getMode() === 'star') {
           const r = newFO.getAttribute('r')
           return {
@@ -407,6 +520,7 @@ export default {
         if (!i) {
           showPanel(false, 'star')
           showPanel(false, 'polygon')
+          showPanel(false, 'starlight');
           return
         }
         while (i--) {
@@ -426,9 +540,17 @@ export default {
             } else {
               showPanel(false, 'polygon')
             }
+          } else if (elem?.getAttribute('shape') === 'starlight') {
+            if (opts.selectedElement && !opts.multiselected) {
+              $id('starlightIntensity').value = elem.getAttribute('intensity');
+              showPanel(true, 'starlight');
+            } else {
+              showPanel(false, 'starlight');
+            }
           } else {
             showPanel(false, 'star')
             showPanel(false, 'polygon')
+            showPanel(false, 'starlight');
           }
         }
       }
